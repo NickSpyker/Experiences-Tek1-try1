c276617952dcd957b3ea30b314d4237157e7c7e3
NickSpyker <nicolas.spijkerman@epitech.eu>
2022-01-31 16:45:25 +0100

    update

--- main.c
+++ main.c
@@ -6,17 +6,17 @@
 
 int main(void)
 {
-    CURL *curl;
+    CURL *handle;
     CURLcode res;
 
-    curl = curl_easy_init();
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "https://bourse.boursorama.com/bourse/action/graph/ws/GetTicksEOD?symbol=1rPCAC&length=1&period=0&guid=");
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-        res = curl_easy_perform(curl);
+    handle = curl_easy_init();
+    if (handle) {
+        curl_easy_setopt(handle, CURLOPT_URL, "https://bourse.boursorama.com/bourse/action/graph/ws/GetTicksEOD?symbol=1rPCAC&length=1&period=0&guid=");
+        curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1L);
+        res = curl_easy_perform(handle);
         if (res != CURLE_OK)
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        curl_easy_cleanup(curl);
+        curl_easy_cleanup(handle);
     }
     return 0;
 }
