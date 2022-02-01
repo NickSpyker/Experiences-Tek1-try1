422f47ecddf3adc2d0f76c81205e4382dea56886
NickSpyker <nicolas.spijkerman@epitech.eu>
2022-02-01 12:16:20 +0100

    update

---
+++ get_data.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+def get_data url
+    system "curl #{url} > out.json && clear"
+    buffer = (open "out.json").read
+    buffer.delete! "azertyuiopqsdfghjklmwxcvbn:\t{}\""
+    buffer = buffer.split ','
+    for i in 0 .. 5
+        buffer[i] = buffer[i].to_f
+    end
+    buffer
+end
--- install_requirements.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install build-essential
-sudo apt-get install gcc
-sudo apt-get install libcurl4-openssl-dev
-sudo apt-get install libcurl4-gnutls-dev
