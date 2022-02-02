b15f9c77b510868969c3173a0a54a3aea86f9e90
NickSpyker <nicolas.spijkerman@epitech.eu>
2022-02-02 11:40:34 +0100

    update

--- brain.rb
+++ brain.rb
@@ -9,13 +9,14 @@ def play all_data, buffer, mm, mm_history, start, var, game, money, cash
     if mm_history.size > 5
         if (mm_history[var] >= mm_history[var - 1] >= mm_history[var - 2] >= mm_history[var - 3])
             if (mm_history[var] - 5 < buffer[4] && buffer[4] < mm_history[var] + 5)
-                cash += 100 if money >= 100
+                cash += buffer[4] if money >= buffer[4]
+                money -= buffer[4]
                 game = "Bought..."
             end
         end
         if (mm_history[var] <= mm_history[var - 1] <= mm_history[var - 2] <= mm_history[var - 3]) && (mm_history[var] < buffer[4])
             game = "Wait..."
-            money += 100
+            money += buffer[4]
             cash = 0
         end
     end
